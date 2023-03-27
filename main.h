b4e3627af4e21e49103de97f1dd7f2c68edcccba
Alvin Kiiru <alvinkiiru@gmail.com>
2023-03-27 14:59:28 +0300

    main.h

--- main.h
+++ main.h
@@ -3,8 +3,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+#define S_LONG 2
+#define S_SHORT 1
 
 struct fmt
 
